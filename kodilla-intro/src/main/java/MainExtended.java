fcd0400cef7d4ab49c90357dda5f17969f248ba5
Aga Gorny <aga.gorny33@gmail.com>
2025-02-05 01:14:03 +0100

    HelloWorld

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,22 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myInitials = "AG";
+        System.out.println(myInitials);
+
+        int myAge = 42;
+        System.out.println(myAge);
+
+        double myHeight = 1.82;
+        System.out.println(myHeight);
+
+        char favoriteLetter = 'A';
+        System.out.println(favoriteLetter);
+
+        String favoriteColor = "czerwony";
+        System.out.println(favoriteColor);
+
+        boolean likeJava = true;
+        System.out.println(likeJava);
+
+    }
+}
