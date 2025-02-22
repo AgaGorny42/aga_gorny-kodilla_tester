e76e9ed76b4c4d6804ccd0543c99d5225079c719
Aga Gorny <aga.gorny33@gmail.com>
2025-02-22 15:24:36 +0100

    Zadanie modul 3.5

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/basic_assertion/Application.java
@@ -0,0 +1,34 @@
+package com.kodilla.abstracts.basic_assertion;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+
+        int sumResult = calculator.sum(a, b);
+        boolean correct = ResultChecker.assertEquals(13, sumResult);
+        if (correct) {
+            System.out.println("Metoda sum dziala poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie dziala poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int subtractResult = calculator.subtract(a, b);
+        boolean correct1 = ResultChecker.assertEquals(-3, subtractResult);
+        if(correct) {
+            System.out.println("Metoda subtract dziala poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie dziala poprawnie dla liczb " + a + " i " + b);
+        }
+
+        double powResult = calculator.pow(a, b);
+        boolean correct2= ResultChecker.assertEquals((double) 390.625, powResult);
+        if(correct) {
+            System.out.println("Metoda pow dziala poprawnie dla liczb " + a + " i " + b);
+        }else {
+            System.out.println("Metoda pow dziala poprawnie dla liczb " + a + " i " + b);
+        }
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/basic_assertion/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla.abstracts.basic_assertion;
+
+public class Calculator {
+
+    int a;
+    int b;
+
+    public int sum(int a, int b){
+        return a + b;
+
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double pow(int a, int b) {
+        return Math.pow(a, b);
+    }
+}
