9a932b6b977d06a5410d8b739ca01a34a0ee7e7b
Aga Gorny <aga.gorny33@gmail.com>
2025-02-22 20:32:42 +0100

    Zadanie modul 3.5 poprawki w klasie Application

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/basic_assertion/Application.java
@@ -17,18 +17,18 @@ public class Application {
 
         int subtractResult = calculator.subtract(a, b);
         boolean correct1 = ResultChecker.assertEquals(-3, subtractResult);
-        if(correct) {
+        if(correct1) {
             System.out.println("Metoda subtract dziala poprawnie dla liczb " + a + " i " + b);
         } else {
             System.out.println("Metoda subtract nie dziala poprawnie dla liczb " + a + " i " + b);
         }
 
         double powResult = calculator.pow(a, b);
-        boolean correct2= ResultChecker.assertEquals((double) 390.625, powResult);
-        if(correct) {
-            System.out.println("Metoda pow dziala poprawnie dla liczb " + a + " i " + b);
+        boolean correct2= ResultChecker.assertEquals((double) 390625, powResult);
+        if(correct2) {
+            System.out.println("Metoda pow dziala poprawnie dla liczb " + a + " i " + b + " Poprawny wynik: " + powResult);
         }else {
-            System.out.println("Metoda pow dziala poprawnie dla liczb " + a + " i " + b);
+            System.out.println("Metoda pow nie dziala poprawnie dla liczb " + a + " i " + b);
         }
     }
 }
