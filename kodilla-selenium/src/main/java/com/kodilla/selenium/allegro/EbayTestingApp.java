ac9b81af07671450dba39ec0947cbcf94e102d80
Aga Gorny <aga.gorny33@gmail.com>
2025-05-05 16:39:54 +0200

    Zadanie: Cwiczymy XPath 13.4

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/EbayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/EbayTestingApp.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class EbayTestingApp {
     public static void main(String[] args) {
@@ -15,8 +18,27 @@ public class EbayTestingApp {
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.ebay.pl/");
 
-        WebElement inputField = driver.findElement(By.xpath("//*[@id=\"gh-ac-wrap\"]/input"));
-        inputField.sendKeys("mavic mini");
-        inputField.submit();
+        WebElement select = driver.findElement(By.cssSelector("#gh-cat"));
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        select.click();
+
+        WebElement select2 = driver.findElement(By.cssSelector(".gh-search-categories"));
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@class=\"gh-search-categories\"]/option"));
+        elements.get(1).click();
+
+        String text = elements.get(1).getText();
+        System.out.println(text);
+
+        for(int i=0; i < elements.size(); i++) {
+            System.out.println(elements.get(i).getText());
+        }
+
+        WebElement clickFind = driver.findElement(By.className("gh-search-button__label"));
+        clickFind.click();
+
+
+//        WebElement inputField = driver.findElement(By.xpath("//*[@id=\"gh-ac-wrap\"]/input"));
+//        inputField.sendKeys("mavic mini");
+//        inputField.submit();
     }
 }
